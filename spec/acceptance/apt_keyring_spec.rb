63883a8696bb1c58cd265a7f505dfb96707ca058
praj1001 <prajjwal.gupta@perforce.com>
2023-10-24 20:37:09 +0530

    removed tabs

--- spec/acceptance/apt_keyring_spec.rb
+++ spec/acceptance/apt_keyring_spec.rb
@@ -5,9 +5,9 @@ require 'spec_helper_acceptance'
 describe 'apt::keyring' do
   context 'when using default values and source specified explicitly' do
     keyring_pp = <<-MANIFEST
-    	apt::keyring { 'puppetlabs-keyring.gpg':
-  			source => 'https://apt.puppetlabs.com/keyring.gpg',
-    	}
+     apt::keyring { 'puppetlabs-keyring.gpg':
+  	 source => 'https://apt.puppetlabs.com/keyring.gpg',
+    }
     MANIFEST
 
     it 'applies idempotently' do
